6d4de2f7bcc870336dfca3432f1ca99c92a53665
wendian_lai <wendian_lai@163.com>
2020-09-07 08:44:14 +0800

    add density_plot

---
+++ density_plot/density.m
@@ -0,0 +1,58 @@
+%% Example: Scatter plot with color indicating data density
+% Wendain Lai
+% scatter plot function links: https://jp.mathworks.com/matlabcentral/fileexchange/8577-scatplot
+
+%% 读取数据
+clc ; clear ;
+load *.mat  % load data, contain x: true/synthetic data,   y: predicted/estimated data
+
+r = corrcoef(x, y);
+r2 = r(1,2)^2;
+MAPE = mean(abs((y - x)./ x))*100; 
+RMSE = sqrt(mean((x - y) .^ 2));
+Mean_ratio = mean(y ./ x);
+
+xmin = 10^-3;
+xmax = 10;
+ymin = 10^-3;
+ymax = 10;
+%% validation
+method = 'circles';
+radius = sqrt((range(x)/30)^2 + (range(y)/30)^2);
+N = 100; % size of the square mesh
+n = 5;
+po = 1;
+ms = 22;
+
+scatplot(x, y, method,radius,N,n,po,ms);
+set(gca, 'yscale', 'log', 'xscale', 'log', 'xlim', [xmin xmax], 'ylim', [ymin ymax]...
+    , 'box', 'on','fontname','Times New Roman','fontsize', 14,'linewidth', 1.2,...
+    'xcolor', 'k', 'ycolor', 'k');
+xlabel(' In situ measured C (mg m^{-3}) ','fontname','Times New Roman','color','k', 'fontsize', 20);
+ylabel([algae ' (mg m^{-3})'],'fontname','Times New Roman','color','k', 'fontsize', 20);
+axis square
+
+set(gcf, 'position', [400 150 500 500]);  % delete the surrounding white space
+
+%  set the 1:1 line
+hold on
+oneLine = plot([xmin xmax], [ymin ymax], 'r', 'linewidth', 1.2);
+legend = legend(oneLine, '1:1 line');
+set(legend,'position', 'best','box','off','fontname','Times New Roman', 'fontsize', 18,...
+    'color','b');
+
+% assessment index
+sR2 = sprintf('R^{2} = %0.2f', r2);
+sMAPE = sprintf('MAPE = %0.2f ', MAPE);
+sRMSE = sprintf('RMSE = %0.3f mg m^{-3}', RMSE);
+sMean_ratio = sprintf('Mean ratio = %0.2f', Mean_ratio);
+text(0.1, 0.8,{sR2; [sMAPE '%'] ; sRMSE ; sMean_ratio}, ...
+    'fontname','Times New Roman', 'fontsize', 18, ...
+    'units', 'normalized')  % use normalized units
+
+%% 图片保存
+filedir = '';  % AP(absolute path)
+filename = ['example.tif'];
+print(gcf, '-dtiffn', '-r600', [ filedir filename]);   % figure with dpi = 600
+close all;
+
